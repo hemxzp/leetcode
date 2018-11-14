4935d529ea4b7eb4a37167487b1b3a0e5c4b0358
hmx <1316437633@qq.com>
2018-11-15 03:17:38 +0800

    2018-11-15 leetcode 50 一行写完，难怪很多人踩，二刷认真写一下

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 _*_
+class Solution:
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return pow(x,n)
